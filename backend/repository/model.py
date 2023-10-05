a37a3d5a9011f0f7993dc7b9301dad8a92233374
moonjunyoung <kkunyeot0729@gmail.com>
2023-10-05 21:07:22 +0900

    feat:repository.model/user테이블 모델을 추가한다

---
+++ backend/repository/model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = "user"
+    id = Column("id", Integer, primary_key=True)
+    identifier = Column(String)
+    password = Column(String)
+
+    def __init__(self, id, identifier, password):
+        self.id = id
+        self.identifier = identifier
+        self.password = password
