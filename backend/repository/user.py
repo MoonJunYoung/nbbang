542327798d714c8a3d31d5587b156a9df7a1075d
moonjunyoung <kkunyeot0729@gmail.com>
2023-10-05 21:08:59 +0900

    feat:repository.user//디비 사용자 생성 기능을 추가한다

---
+++ backend/repository/user.py
@@ -0,0 +1,13 @@
+from domain.user import User
+from repository.connector import MysqlCreate
+from repository.model import UserModel
+
+
+class UserRepository:
+    def create(self, user: User) -> None:
+        user_model = UserModel(
+            id=None,
+            identifier=user.identifier,
+            password=user.password,
+        )
+        MysqlCreate(user_model).run()
