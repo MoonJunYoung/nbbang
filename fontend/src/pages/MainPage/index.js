2817060abc5636b2cbe24236bbdb66e44f981f11
dnfbd <dnfbd@naver.com>
2023-10-06 14:05:45 +0900

    회원가입/로그인 기능을 추가한다

---
+++ fontend/src/pages/MainPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import Nav from '../../components/Nav'
+
+const Container = styled.div`
+  
+`
+
+const MainPage = () => {
+  return (
+    <Container>
+      <Nav />
+    </Container>
+  )
+}
+
+export default MainPage
