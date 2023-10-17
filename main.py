a424c289eee3540c00bee416e4c7f093111b10b2
moonjunyoung <kkunyeot0729@gmail.com>
2023-10-17 14:15:01 +0900

    fix:CORS 버그를 수정한다

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ from backend.presentation.member import MemberPresentation
 from backend.presentation.metting import MeetingPresentation
 from backend.presentation.payment import PaymentPresentation
 from backend.presentation.user import UserPresentation
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(UserPresentation.router)
-app.include_router(MeetingPresentation.router)
-app.include_router(MemberPresentation.router)
-app.include_router(PaymentPresentation.router)
-from fastapi.middleware.cors import CORSMiddleware
 
 origins = ["*"]
 
@@ -22,3 +17,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(UserPresentation.router)
+app.include_router(MeetingPresentation.router)
+app.include_router(MemberPresentation.router)
+app.include_router(PaymentPresentation.router)
