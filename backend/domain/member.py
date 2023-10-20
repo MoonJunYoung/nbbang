4970982a07f03510f867d8c8b68028c04180a8c8
moonjunyoung <kkunyeot0729@gmail.com>
2023-10-20 15:47:58 +0900

    fix:결제내역에 포함된 멤버는 삭제를 못하도록 예외를 발생시킨다

--- backend/domain/member.py
+++ backend/domain/member.py
@@ -1,6 +1,16 @@
+from backend.domain.payment import Payment
+from backend.exceptions import PaymentInMemberDeleteExcption
+
+
 class Member:
     def __init__(self, id, name, leader, meeting_id) -> None:
         self.id = id
         self.name = name
         self.leader = leader
         self.meeting_id = meeting_id
+
+    def check_in_payment(self, payments: list[Payment]):
+        for payment in payments:
+            for attend_member_id in payment.attend_member_ids:
+                if self.id == attend_member_id:
+                    raise PaymentInMemberDeleteExcption
