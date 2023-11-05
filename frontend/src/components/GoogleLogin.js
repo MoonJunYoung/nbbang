af79d8e4b9443d0a9fd481fcce9d30ee17c48afb
dnfbd <dnfbd@naver.com>
2023-11-05 23:34:20 +0900

    구글 로그인 기능을 수정한다

--- frontend/src/components/GoogleLogin.js
+++ frontend/src/components/GoogleLogin.js
@@ -18,25 +18,26 @@ const GooglesContainer = styled.div`
 `;
 
 const Button = styled.button`
-      width: 230px;
-    height: 40px;
-    background: white;
-    border: 1px solid paleturquoise;
-    border-radius: 10px;
+  width: 230px;
+  height: 40px;
+  background: white;
+  border: 1px solid paleturquoise;
+  border-radius: 10px;
 `;
 
 const Googles = () => {
   const handleGoogleLogin = () => {
-    window.location.href =
-      "https://accounts.google.com/o/oauth2/v2/auth?response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&client_id=470039216193-568hnttd1011ddmc5j22nqia9rcjm1ah.apps.googleusercontent.com&redirect_uri=https://nbbang.shop/signd";
+    if (navigator.userAgent.includes("KAKAOTALK")) {
+      alert("구글 SNS 로그인을 지원하지 않는 브라우저 앱 입니다. 다른 브라우저를 이용해주세요!");
+    } else {
+      window.location.href =
+        "https://accounts.google.com/o/oauth2/v2/auth?response_type=token&scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&client_id=470039216193-568hnttd1011ddmc5j22nqia9rcjm1ah.apps.googleusercontent.com&redirect_uri=https://nbbang.shop/signd";
+    }
   };
 
   return (
-    <GooglesContainer onClick={handleGoogleLogin} >
-        <img 
-          alt='N/1'
-          src='/images/google.png'
-        />
+    <GooglesContainer onClick={handleGoogleLogin}>
+      <img alt="google" src="/images/google.png" />
       <Button>구글 로그인</Button>
     </GooglesContainer>
   );
