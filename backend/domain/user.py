6cc5d1b4f8200233e0527d8e9fbd8d07792e6473
moonjunyoung <kkunyeot0729@gmail.com>
2023-11-07 16:18:35 +0900

    fix:유저의 기본 계좌정보를 저장할떄 암호화를 진행한다

--- backend/domain/user.py
+++ backend/domain/user.py
@@ -1,3 +1,13 @@
+import os
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from dotenv import load_dotenv
+
+load_dotenv()
+secret_key = bytes(os.environ.get("ENCRYPT_KEY"), "UTF-8")
+
+
 class User:
     def __init__(self, id, name, platform_id, platform, bank, account_number, kakao_id) -> None:
         self.id = id
@@ -7,6 +17,10 @@ class User:
         self.bank = bank
         self.account_number = account_number
         self.kakao_id = kakao_id
+        if isinstance(self.account_number, str) and isinstance(self.bank, str):
+            self._encrypt_account_number_data()
+        elif isinstance(self.account_number, bytes) and isinstance(self.bank, bytes):
+            self._dncrypt_account_number_data()
 
     def update_account_number(self, bank, account_number):
         self.bank = bank
@@ -14,3 +28,21 @@ class User:
 
     def update_kakao_id(self, kakao_id):
         self.kakao_id = kakao_id
+
+    def _encrypt_account_number_data(self):
+        self.account_number = self.__aes_encrypt(secret_key, self.account_number)
+        self.bank = self.__aes_encrypt(secret_key, self.bank)
+
+    def _dncrypt_account_number_data(self):
+        self.account_number = self.__aes_decrypt(secret_key, self.account_number)
+        self.bank = self.__aes_decrypt(secret_key, self.bank)
+
+    def __aes_encrypt(self, key, plaintext):
+        cipher = AES.new(key, AES.MODE_ECB)
+        ciphertext = cipher.encrypt(pad(plaintext.encode("utf-8"), AES.block_size))
+        return ciphertext
+
+    def __aes_decrypt(self, key, ciphertext):
+        cipher = AES.new(key, AES.MODE_ECB)
+        decrypted_data = unpad(cipher.decrypt(ciphertext), AES.block_size)
+        return decrypted_data.decode("utf-8")
